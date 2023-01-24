5d905600dcca41ea12c66eba9f86aa861747ffec
ychkydyk <zavdv1@gmail.com>
2023-01-25 00:47:08 +0300

    rebuild Router logick

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,34 @@
+import React, {Fragment} from 'react';
+
 import "./app.scss"
 import Home from "./pages/home/Home";
 import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
 import Login from "./pages/login/Login";
 import {
-  BrowserRouter as Router,
-  Route,
-  Navigate,
+    BrowserRouter as Router,
+    Route,
+    Navigate, Routes,
 } from "react-router-dom";
 
 const App = () => {
     const user = true;
   return  (
       <Router>
-        <Route>
-          <Route exact path="/">
-            {user ? <Home /> : <Navigate to="/register" />}
-          </Route>
-          <Route path="/register">
-            {!user ? <Register /> : <Navigate to="/" />}
-          </Route>
-          <Route path="/login">{!user ? <Login /> : <Navigate to="/" />}</Route>
+          <Fragment>
+        <Routes>
+          <Route exact path="/"  element= {user ? <Home /> : <Navigate to="/register" />} />
+          <Route path="/register" element={!user ? <Register /> : <Navigate to="/" />}/>
+          <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />}  />
           {user && (
               <>
-                <Route path="/movies">
-                  <Home type="movie" />
-                </Route>
-                <Route path="/series">
-                  <Home type="series" />
-                </Route>
-                <Route path="/watch">
-                  <Watch />
-                </Route>
+                <Route path="/movies" element={<Home type="movie" />}></Route>
+                <Route path="/series" element={<Home type="series" />}></Route>
+                <Route path="/watch" element={<Watch />}></Route>
               </>
           )}
-        </Route>
+        </Routes>
+          </Fragment>
       </Router>
   );
 };
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,8 +2,34 @@ import "./home.scss"
 import Navbar from "../../components/navbar/Navbar";
 import Featured from "../../components/featured/Featured";
 import List from "../../components/list/List";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 const Home = ({type}) => {
+    const [lists, setLists] = useState([]);
+    const [genre, setGenre] = useState(null);
+
+    useEffect(() => {
+        const getRandomLists = async () => {
+            try {
+                const res = await axios.get(
+                    `lists${type ? "?type=" + type : ""}${
+                        genre ? "&genre=" + genre : ""
+                    }`,{
+                        headers: {
+                            token: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzY2YyOTg0NjY2MjJjZDEyMzY0NzNlOCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3NDU5NTkyOSwiZXhwIjoxNjc1MDI3OTI5fQ.liA9uKKlOyNSTWAMiBVbjAt8Vbx2ezRudhjD6GGYJ6g"
+                            }
+                        }
+                    );
+                    setLists(res.data);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        getRandomLists();
+    }, [type, genre]);
+
+
     return (
         <div className="home">
             <Navbar/>
